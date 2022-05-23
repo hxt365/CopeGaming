e68dd8fc4842e5f696db9c6fde8683ed7a4f8ceb
Truong Hoang <hxt365@gmail.com>
2022-05-23 18:57:37 +0300

    allows providers to get their own servers status with ownerID

--- coordinator/app/api/provider/provider.go
+++ coordinator/app/api/provider/provider.go
@@ -25,19 +25,23 @@ type GetProviderListResp struct {
 }
 
 func GetProviderList(hub *client.Hub, w http.ResponseWriter, r *http.Request) {
+	ownerID := r.URL.Query().Get("owner")
+
 	var providers []*Provider
 
 	for _, p := range hub.GetProviders() {
-		providers = append(providers, &Provider{
-			ID:         p.ID,
-			HostName:   p.Provider.HostName,
-			Platform:   p.Provider.Platform,
-			CpuName:    p.Provider.CpuName,
-			CpuNum:     p.Provider.CpuNum,
-			MemSize:    p.Provider.MemSize,
-			CpuPercent: p.Provider.CpuPercent,
-			MemPercent: p.Provider.MemPercent,
-		})
+		if ownerID == "" || p.Provider.OwnerID == ownerID {
+			providers = append(providers, &Provider{
+				ID:         p.ID,
+				HostName:   p.Provider.HostName,
+				Platform:   p.Provider.Platform,
+				CpuName:    p.Provider.CpuName,
+				CpuNum:     p.Provider.CpuNum,
+				MemSize:    p.Provider.MemSize,
+				CpuPercent: p.Provider.CpuPercent,
+				MemPercent: p.Provider.MemPercent,
+			})
+		}
 	}
 
 	resp := response.Response{
